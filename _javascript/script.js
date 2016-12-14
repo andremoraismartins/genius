2f68e4c159bb4b833394f5f953c9d45e3ac12565
André Martins <andre.morais.martins@gmail.com>
2016-12-14 10:26:02 -0200

    Adição dos marcadores, mouseup e mouse down as tds
    Adição dos marcadores de nível e situação. Adição do eventos mouseup e
    mousedown as tds. Adição da função trocarCor.

--- _javascript/script.js
+++ _javascript/script.js
@@ -1,61 +1,104 @@
 var nivel = [];
 
-function piscar(botao, callback){
-    var td, corNova, corAntiga;
+function trocarCor(botao){
     switch(parseInt(botao)){
         case 0:
             td = $("td#botaoVerde");
-            corNova = "green";
-            corAntiga = "rgba(0,255,0, 0.3)";
+            corNova = "rgb(0, 255, 0)";
+            corAntiga = "rgba(0, 255, 0, 0.3)";
             break;
         case 1:
             td = $("td#botaoAmarelo");
-            corNova = "yellow";
-            corAntiga = "rgba(255,255,0, 0.3)";
+            corNova = "rgb(255, 255, 0)";
+            corAntiga = "rgba(255, 255, 0, 0.3)";
             break;
         case 2:
             td = $("td#botaoVermelho");
-            corNova = "red";
-            corAntiga = "rgba(255,0,0, 0.3)";
+            corNova = "rgb(255, 0, 0)";
+            corAntiga = "rgba(255, 0, 0, 0.3)";
             break;
         case 3:
             td = $("td#botaoAzul");
-            corNova = "blue";
-            corAntiga = "lightblue";
+            corNova = "rgb(0, 0, 255)";
+            corAntiga = "rgba(0, 0, 255, 0.3)";
     }
-    td.css("background", corNova);
-    setTimeout(function(){
-        td.css("background", corAntiga);
-        if(typeof callback == "function") 
-            callback();       
-    }, 500);
+    if (td.css("background-color") == corNova)
+        td.css("background-color", corAntiga);
+    else
+        td.css("background-color", corNova);
 }
 
-function piscarSequencia(seq){ 
+function piscar(botao, callback){
+    trocarCor(botao);
+    setTimeout(function(){
+        trocarCor(botao);
+        if(typeof callback == "function") 
+            callback();       
+    }, 500);    
+}
+
+function piscarSequencia(seq, callback){ 
     var sequencia = seq.slice(0);   
     if (sequencia.length)
         piscar(sequencia[0], function(){
             sequencia.shift();
-            piscarSequencia(sequencia);
+            piscarSequencia(sequencia, callback);
         });
+    else if (typeof callback == "function")
+        callback();
 }
 
 function numeroAleatorio(){
     return Math.floor((Math.random() * 4) + 0);
 }
 
-function gerarProximoNivel(){
+function gerarProximoNivel(callback){
     var proxNivel = nivel.length;
     nivel.push(numeroAleatorio());
-    piscarSequencia(nivel);
+    piscarSequencia(nivel, callback);
 }
 
 $(document).ready(function(){
-    $("td").click(function(){
-        piscar($(this).attr("data-index"));
+    var index = 0,
+        liberarClick = false;
+    $("td").mouseup(function(){
+        if (liberarClick){
+            var clique = $(this).attr("data-index");
+            trocarCor(clique);
+            if (clique == nivel[index]){
+                if (index == (nivel.length - 1)){
+                    $("h3#situacao").html("Correto! Próximo nível!");
+                    $("#btnIniciar").val("Iniciar Nível");
+                    $("#btnIniciar").prop("disabled", false);
+                    liberarClick = false;
+                    return;
+                }
+                index++;                        
+            }
+            else{
+                $("h3#situacao").html("Você perdeu!");
+                $("#btnIniciar").val("Reiniciar");
+                $("#btnIniciar").prop("disabled", false);
+                nivel = [];
+                liberarClick = false;
+            }
+        }
+    }).mousedown(function(){
+        if (liberarClick){
+            var clique = $(this).attr("data-index");
+            trocarCor(clique);
+        }
     });
 
-    $("#btnExecutar").click(function(){
-        gerarProximoNivel();
+    $("#btnIniciar").click(function(){
+        index = 0;
+        liberarClick = false;
+        $("#btnIniciar").prop("disabled", true);
+        $("h3#situacao").text("Preste atenção nas cores!");
+        $("h2#nivelAtual").text("Nível Atual: " + (nivel.length + 1)); 
+        gerarProximoNivel(function(){
+            liberarClick = true;
+            $("h3#situacao").text("Sua vez!");
+        });
     });
 });
